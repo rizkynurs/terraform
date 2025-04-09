7e7872d0792bec46ae52393aa3019ffc1358763c
rizkynurachmad <rizky.nurachmad@detik.com>
2025-04-09 09:23:13 +0700

    add file provisioning

--- huawei/multiple-ecs/main.tf
+++ huawei/multiple-ecs/main.tf
@@ -14,17 +14,17 @@ data "huaweicloud_vpc_subnet" "mynet" {
   name = var.subnet_name
 }
 
-resource "huaweicloud_compute_keypair" "ssh_key" {
-  name       = "my-ssh-key"            # Key pair name
-  public_key = file("~/.ssh/id_rsa.pub") # Path to your local SSH public key
-}
+# resource "huaweicloud_compute_keypair" "ssh_key" {
+#   name       = "my-ssh-key"            # Key pair name
+#   public_key = file("~/.ssh/id_rsa.pub") # Path to local SSH public key
+# }
 
 # VM Instance Resource
 resource "huaweicloud_compute_instance" "vm" {
   count              = 2
   name               = "test-centos0${count.index + 3}"
   image_id           = var.huaweicloud_images["centos"].id
-  flavor_id          = "s7n.small.1" #1 cpu 1gb memory, for more detail check on https://support.huaweicloud.com/intl/en-us/productdesc-ecs/ecs_01_0014.html
+  flavor_id          = "s7n.medium.2" #1 cpu 1gb memory, for more detail check on https://support.huaweicloud.com/intl/en-us/productdesc-ecs/ecs_01_0014.html
   availability_zone  = data.huaweicloud_availability_zones.az1.names[0]
   security_group_ids = [var.secgroup["sg-core-service"].id]
   admin_pass         = var.admin_pass
@@ -33,24 +33,54 @@ resource "huaweicloud_compute_instance" "vm" {
 
   network {
     uuid = data.huaweicloud_vpc_subnet.mynet.id
-    fixed_ip_v4 = "10.250.0.8${count.index + 7}" #start from 0.87
+    fixed_ip_v4 = "10.250.0.8${count.index + 7}" #mulai dari 0.87
   }
 
   tags = {
-    ip-db-collector = "ip-db-collector"
+    test-centos = "fe"
   }
 
   enterprise_project_id = var.project["core-service"].id
+  
+  # key_pair = huaweicloud_compute_keypair.ssh_key.name
 
-  # Use the key pair created in Terraform
-  key_pair = huaweicloud_compute_keypair.ssh_key.name
+  # Use ansible for provisioning
+  # Uncomment the following block if you want to use ansible for provisioning
+  # provisioner "local-exec" {
+  #   command = <<-EOT
+  #     source /Users/rizky/ansible-env/bin/activate
+  #     ansible-playbook -i "${self.access_ip_v4}," /Users/rizky/sky-cloud/ansible/playbook/preinstall-enhance.yml --extra-vars "ansible_host=${self.access_ip_v4} ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa"
+  #   EOT
+  # }
 
-  provisioner "local-exec" {
-    command = <<-EOT
-      source /Users/rizky/ansible-env/bin/activate
-      ansible-playbook -i "${self.access_ip_v4}," /Users/rizky/sky-cloud/ansible/playbook/preinstall-enhance.yml --extra-vars "ansible_host=${self.access_ip_v4} ansible_ssh_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa"
-    EOT
-  }
+  # Use file provioning to run a script on the remote instance
+  # Uncomment the following block if you want to use file provisioning
+  # provisioner "file" {
+  #   source      = "/Users/rizky/sky-cloud/terraform/huawei/multiple-ecs/config/preinstall.sh"  # Path to your local script file
+  #   destination = "/tmp/preinstall.sh"  # Location on the remote instance
+
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "root" 
+  #     private_key = file("~/.ssh/id_rsa")  # Path to your private SSH key
+  #     host        = "${self.access_ip_v4}"
+  #   }
+  # }
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "chmod +x /tmp/preinstall.sh",  # Make the script executable
+  #     "/tmp/preinstall.sh"            # Run the script
+  #   ]
+
+  #   connection {
+  #     type        = "ssh"
+  #     user        = "root"  # Replace with the correct username for your instance
+  #     private_key = file("~/.ssh/id_rsa")  # Path to your private SSH key
+  #     host        = "${self.access_ip_v4}"
+  #   }
+  # }
+
 }
 
 # Adding an EVS Disk 
